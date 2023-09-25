4ff50d0674d3e34c7e17760cfe720e5a13ffd77e
MD. Shahadot Hossain Rahat <shrhossain786@gmail.com>
2023-09-25 19:10:44 +0600

    got the firebase data

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
-import React from 'react';
+import {firebase} from '@react-native-firebase/database';
 
-const App = () => {
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const databaseRef = firebase.database().ref('/Movies');
+
+    const onDataChange = snapshot => {
+      const newData = snapshot.val();
+      setData(newData);
+    };
+
+    databaseRef.on('value', onDataChange);
+    return () => {
+      databaseRef.off('value', onDataChange);
+    };
+  }, []);
+
+  // console.log(JSON.stringify(data, null, 2));
+
   return (
     <View>
       <Text>App</Text>
     </View>
   );
-};
+}
 
 export default App;
